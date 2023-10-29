4293af39f566fd2506fd24b1b6b7c3a186debae7
WUALTER AUGUSTO HERNÁNDEZ <141308810+2002August@users.noreply.github.com>
2023-10-29 12:28:50 -0600

    Add files via upload

---
+++ proyecto final/src/java/DAO/LoginDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import modelo.Conexion;
+import Model.TrabajadorModel;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author victor
+ */
+public class LoginDAO {
+    int rspta=0;
+    String sql="";
+    ResultSet rs=null;
+    conexion cn=new conexion();
+    public int validarLogin(TrabajadorModel tm) throws Exception{
+        sql="SELECT COUNT(CODTRABAJADOR) AS cantidad FROM `trabajador` WHERE USUARIO='"+tm.getUsuario()+"' AND CLAVE='"+tm.getClave()+"'";
+        rs=cn.ejecutarConsulta(sql);
+        while(rs.next()){
+            rspta=rs.getInt("cantidad");
+        }
+    return  rspta;
+    }
+    
+}
